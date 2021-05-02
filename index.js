b6dcd9b99cccabb0d4fc01a39395e21ba758ce4b
KimberN <kimberly.k.noyes@gmail.com.com>
2021-05-02 15:08:35 -0600

    first js

---
+++ index.js
@@ -0,0 +1,14 @@
+var x = 1;
+var y = 5;
+console.log(x + y); //6
+console.log(x - y); //-4
+console.log(x / y); //0.2
+console.log(x * y); //5
+var q = "Hello ";
+var w = "World";
+var e = q + w;
+console.log(e); //Hello World
+var p = "My Favorite Food is ";
+var o = "Asparagus";
+var i = p + o;
+console.log(i); //My Favorite Food is Asparagus
